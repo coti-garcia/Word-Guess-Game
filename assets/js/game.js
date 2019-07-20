8e2840ad3eb1f9d02ef431e7254f162584a4f6ec
coti_garcia <javiera.garcialatorre@gmail.com>
2019-07-20 01:39:48 -0400

    loop generate spaces for letters

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
     let wins = 0;
-    let word = "hola";
+    let word = "tito";
     let wordLength = word.length;
     let guessesRemaining = wordLength;
-    
+
     $("#wins").append(wins);
     $("#guesses-remaining").append(guessesRemaining);
 
     for( i = 0; i < wordLength; i++){
         $("#word").append("<span>");
+        console.log(word[i]); // Result Letters of the word.
     };
 
-    
 });
