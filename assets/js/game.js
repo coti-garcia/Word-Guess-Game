ba6e6f26ebb9008ccafc2ad28c7e1d3603294a10
coti_garcia <javiera.garcialatorre@gmail.com>
2019-07-22 15:06:44 -0400

    change from one game to the next one

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,57 +1,84 @@
 $(document).ready(function(){
     let wins = 0;
     $("#wins").append(wins);
-    // Groups of name 1
-    const kalho = {
+    const letters = ["q", "w" , "e", "r", "t", "y","u","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"]
+
+    // Groups of words
+    const words = [{
             name : "kalho",
             active : true
-        };
-
-    const spencer = {
+        },
+        {
             name : "spencer",
             active : false
-        };
-    
-    const  degeneres = {
+        },
+        {
             name : "degeneres",
             active : false
-        };
-
-    const obama = {
+        },
+        
+        {
             name : "obama",
             active : false
-        };
-    
-    const streep = {
+        },{
             name : "streep",
             active : false
-        };
-    
+        }
+    ]
+    let wordIndex = 0;
+    //const words = [kalho, spencer, degeneres, obama, streep];
+    //console.log(words);
+
 
     function game(word){
+        $("#used-letters").empty();
         let wordLength = word.length;
         let guessesRemaining = wordLength * 2;
         let imagePath = `./assets/images/${word}.jpg`;
+        let userGuessesArr = [];
+
         //Forloop for span letters word
-            for( i = 0; i < wordLength; i++){
-                $("#word").append("<span>");
-                console.log(word[i]); // Result Letters of the word.
-            };
+        for( i = 0; i < wordLength; i++){
+            $("#word").append("<span>");
+            // console.log(word[i]); // Result Letters of the word.
+        };
         $("#word-image").attr("src", imagePath);
         $("#guesses-remaining").append(guessesRemaining);
     };
 
-    game(degeneres.name);
+ 
+
+    // $(document).keyup(function(event){
+    //     const userGuess = event.key;
+    //     console.log(userGuess);
+    //     $("#used-letters").append(`<span>${userGuess}</span>`);
+        
+    // });
 
     $(document).keyup(function(event){
-        const userGuess = event.key;
+        let userGuess = event.key;
         console.log(userGuess);
-        $("#used-letters").append(`<span>${userGuess}</span>`);
+        // if (userGuessesArr.includes(userGuess)){
+        //     alert("Already Guessed! please choose another letter")
+        // }else if (letters.includes(userGuess)){
+        //     userGuessesArr.push(userGuess);
+        //     console.log(userGuessesArr);
+        //     $("#used-letters").append(`<span>${userGuess}</span>`);
+        // } 
+        if(true){
+            wordIndex++;
+            game(words[wordIndex].name);
+        }
     });
 
+    game(words[wordIndex].name);
 
 
-    const words = [kalho, spencer, degeneres, obama, streep];
-    console.log(words);
+
+game(words[wordIndex].name);
+console.log(words[wordIndex].name)
+   // game(kalho.name);
+
+
 
 });
